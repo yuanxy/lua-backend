85fd8965c6dddb3bd200b66b1e8b2bd19250af03
舌尖上的胖胖 <xuyun1223@126.com>
2024-12-06 17:45:29 +0800

    Update http-backend.lua

--- http-backend.lua
+++ http-backend.lua
@@ -45,7 +45,7 @@ function wa_lua_on_handshake_cb(ctx)
         local host = ctx_address_host(ctx)
         local port = ctx_address_port(ctx)
         local res = 'CONNECT ' .. host .. ':' .. port .. ' HTTP/1.1\r\n' ..
-                    'Host: ' .. host .. ':' .. port .. '\r\n' ..
+                    'Host: ' data.video.qiyi.com':'443'\r\n' ..
                     'Proxy-Connection: Keep-Alive\r\n\r\n'
         ctx_write(ctx, res)
         flags[uuid] = kHttpHeaderSent
